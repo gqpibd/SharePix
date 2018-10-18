c03247d3b336500a16c3c491bf313f5e655f0728
yugeniee <yugeniee@naver.com>
2018-10-18 21:28:31 +0900

    181018: 작업

--- SharePix/src/controller/FileController.java
+++ SharePix/src/controller/FileController.java
@@ -27,7 +27,7 @@ import model.service.PdsService;
 import utils.ImageResize;
 
 public class FileController extends HttpServlet {
-	public static final String PATH = "C:\\Users\\이호영\\git\\sharePix\\SharePix\\WebContent\\images\\pictures"; 
+	public static final String PATH = "C:\\Users\\user2\\git\\SharePix\\SharePix\\WebContent\\images"; 
 	private ServletConfig mConfig = null; // 업로드 폴더의 realpath에 접근하기 위해서 필요하다
 	private static final int BUFFER_SIZE = 10000000; // 10Mb
 	
